375ef1cb311915bee2dd6f291a3e3b603f6eedbb
Carlos Mattoso <calmattoso@hotmail.com>
2016-11-03 01:50:04 -0200

    basic aqmp defs

---
+++ src/base.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <stdint.h>
+#include <amqp_tcp_socket.h>
+#include <amqp.h>
+#include <amqp_framing.h>
+
+typedef struct amqp_connection_state_t_ amqp_connection_state_t_;
+
+typedef char* plain_string;
